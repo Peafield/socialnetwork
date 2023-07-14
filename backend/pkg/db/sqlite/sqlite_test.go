e2d8f3270e82924f2ae6652692a1b3151b28a6c1
Harry Gardiner <harry97uk@hotmail.co.uk>
2023-07-14 09:27:15 +0100

    feat(database testing) added new way to test initialiseDatabase

--- backend/pkg/db/sqlite/sqlite_test.go
+++ backend/pkg/db/sqlite/sqlite_test.go
@@ -1,9 +1,6 @@
 package db_test
 
 import (
-	"database/sql"
-	"os"
-	"path"
 	db "socialnetwork/pkg/db/sqlite"
 	"socialnetwork/pkg/models"
 	"testing"
@@ -73,20 +70,73 @@ func TestInitialiseDatabase(t *testing.T) {
 
 func TestCreateDatabase(t *testing.T) {
 	tempPath := t.TempDir()
-	testDBName := "testDB"
-	db.CreateDatabase(tempPath, testDBName)
-	filepath := path.Join(tempPath, testDBName)
-	_, err := os.Stat(filepath)
-	if os.IsNotExist(err) {
-		file, err := os.Create(filepath)
-		if err != nil {
-			t.Fatalf("file does not exist: %s", err)
-		}
-		file.Close()
+
+	testCases := []struct {
+		name          string
+		dbName        string
+		dbDirectory   string
+		dbFileCreated bool
+		dbFileOpened  bool
+		expectError   bool
+	}{
+		{
+			name:          "Database created and opened",
+			dbName:        "mydatabase",
+			dbDirectory:   tempPath,
+			dbFileCreated: true,
+			dbFileOpened:  true,
+			expectError:   false,
+		},
+		{
+			name:          "Database not created",
+			dbName:        "mydatabase",
+			dbDirectory:   tempPath,
+			dbFileCreated: false,
+			dbFileOpened:  false,
+			expectError:   true,
+		},
+		{
+			name:          "Database not created",
+			dbName:        "mydatabase",
+			dbDirectory:   tempPath,
+			dbFileCreated: true,
+			dbFileOpened:  false,
+			expectError:   true,
+		},
 	}
-	db, err := sql.Open("sqlite3", filepath+"/.db?_foreign_keys=on")
-	if err != nil {
-		t.Fatalf("failed to open database: %s", err)
+
+	// Run test cases
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			// Perform the test
+			dbFilePath := &models.BasicDatabaseInit{
+				DBName:    tc.dbName,
+				Directory: tc.dbDirectory,
+			}
+			err := db.InitialiseDatabase(dbFilePath)
+
+			// Assertions
+			if tc.expectError && err == nil {
+				t.Error("Expected an error, but got nil")
+			} else if !tc.expectError && err != nil {
+				t.Errorf("Unexpected error: %s", err)
+			}
+		})
 	}
-	db.Close()
+
+	// db.CreateDatabase(tempPath, testDBName)
+	// filepath := path.Join(tempPath, testDBName)
+	// _, err := os.Stat(filepath)
+	// if os.IsNotExist(err) {
+	// 	file, err := os.Create(filepath)
+	// 	if err != nil {
+	// 		t.Fatalf("file does not exist: %s", err)
+	// 	}
+	// 	file.Close()
+	// }
+	// db, err := sql.Open("sqlite3", filepath+"/.db?_foreign_keys=on")
+	// if err != nil {
+	// 	t.Fatalf("failed to open database: %s", err)
+	// }
+	// db.Close()
 }

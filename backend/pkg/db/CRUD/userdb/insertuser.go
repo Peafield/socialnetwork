b21cf5ac44f12a8c0383053559b3b60f881c569b
Peafield <peterjohncoles@gmail.com>
2023-07-17 21:08:14 +0100

    feat(db): insert user function

--- backend/pkg/db/CRUD/userdb/insertuser.go
+++ backend/pkg/db/CRUD/userdb/insertuser.go
@@ -1,5 +1,70 @@
 package db
 
-// func InsertUser(dbOpener dbmodels.DBOpener, user *dbmodels.User) (dbmodels.User, error) {
-// 	db, err := dbOpener.Open(&dbmodels.SQLDBOpener)
-// }
+import (
+	"fmt"
+	"socialnetwork/pkg/models/dbmodels"
+)
+
+/*
+InsertUser creates a new user record in the database.
+
+It takes as input an object that implements the DBOpener interface and a User object. The DBOpener interface
+should provide methods to retrieve the driver name and the data source name required for establishing a connection
+with the database. The User object contains all necessary data for a new user record.
+
+Upon a successful operation, the function returns the User object that was passed as an input.
+
+Parameters:
+  - dbOpener (dbmodels.DBOpener): An object that provides the driver name and the data source name for the database connection.
+  - user (*dbmodels.User): A pointer to the User object that contains data for the new user record.
+
+Errors:
+  - Returns an error if the database fails to open, if the insert statement fails to prepare or execute, or if the function fails to retrieve the number of affected rows.
+  - Returns an error if no rows were affected when attempting to insert the new user.
+
+Example:
+  - InsertUser is called when a new user is created on the application, to insert the new user's data into the database.
+*/
+func InsertUser(dbOpener dbmodels.DBOpener, user *dbmodels.User) (*dbmodels.User, error) {
+	db, err := dbOpener.Open(dbOpener.GetDriveName(), dbOpener.GetDataSourceName())
+	if err != nil {
+		return &dbmodels.User{}, fmt.Errorf("failed to open database when inserting user: %w", err)
+	}
+	defer db.Close()
+
+	statement, err := db.Prepare(`
+	INSERT INTO Users (
+		user_id,
+		isLoggedIn,
+		email,
+		hashed_password,
+		first_name,
+		last_name, 
+		date_of_birth,
+		avatar_path,
+		display_name,
+		about_me,
+	) VALUES (
+		?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+	)`)
+	if err != nil {
+		return &dbmodels.User{}, fmt.Errorf("failed to prepare insert user statement: %w", err)
+	}
+	defer statement.Close()
+
+	result, err := statement.Exec(user.UserId, user.IsLoggedIn, user.Email, user.HashedPassword, user.FirstName, user.LastName, user.DOB, user.AvatarPath, user.DisplayName, user.AboutMe)
+	if err != nil {
+		return &dbmodels.User{}, fmt.Errorf("failed to execute insert user statement: %w", err)
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return &dbmodels.User{}, fmt.Errorf("failed to retrieve affected rows: %w", err)
+	}
+
+	if rowsAffected == 0 {
+		return &dbmodels.User{}, fmt.Errorf("no rows affected when inserting user: %w", err)
+	}
+
+	return user, nil
+}
--- backend/pkg/db/migrations/0001_create_users_table.up.sql
+++ backend/pkg/db/migrations/0001_create_users_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE Users (
   user_id TEXT UNIQUE NOT NULL PRIMARY KEY,
   isLoggedIn INTEGER NOT NULL DEFAULT 0,
   email TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL,
+  hashed_password TEXT NOT NULL,
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
   date_of_birth DATE NOT NULL,

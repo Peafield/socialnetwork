f7391d5eade556d6ecfc9032bd9c93fc9e0e0346
Harry Gardiner <harry97uk@hotmail.co.uk>
2023-07-27 14:11:23 +0100

    feat(signin handler): added logic for sign in handler, created a validateCredentials function, need to add testing and comments

---
+++ backend/pkg/controllers/validatecredentials.go
@@ -0,0 +1,55 @@
+package controllers
+
+import (
+	"fmt"
+	crud "socialnetwork/pkg/db/CRUD"
+	"socialnetwork/pkg/db/dbutils"
+	"socialnetwork/pkg/helpers"
+	"socialnetwork/pkg/models/dbmodels"
+	"strings"
+)
+
+/*
+ */
+func ValidateCredentials(formData map[string]interface{}) (*dbmodels.User, error) {
+	// Email
+	username_email, ok := formData["username_email"].(string)
+	if !ok {
+		return nil, fmt.Errorf("email is not a string")
+	}
+
+	// Password
+	password, ok := formData["password"].(string)
+	if !ok {
+		return nil, fmt.Errorf("password is not a string")
+	}
+
+	//set conditions
+	conditions := make(map[string]interface{})
+	if strings.Contains(username_email, "@") {
+		conditions["email"] = username_email
+	} else {
+		conditions["display_name"] = username_email
+	}
+
+	conditionStatement := dbutils.ConditionStatementConstructor(conditions)
+
+	//get user data as interface
+	userData, err := crud.SelectFromDatabase(dbutils.DB, "Users", conditionStatement)
+	if err != nil {
+		return nil, fmt.Errorf("error selecting user from database: %s", err)
+	}
+
+	//assert dbmodels.User type
+	user, ok := userData.(dbmodels.User)
+	if !ok {
+		return nil, fmt.Errorf("returned database value is not a User struct: %s", err)
+	}
+
+	//compare passwords
+	err = helpers.CompareHashedPassword(user.HashedPassword, password)
+	if err != nil {
+		return nil, fmt.Errorf("inputted password incorrect: %s", err)
+	}
+	return &user, nil
+}
--- backend/pkg/routehandlers/signinhandler.go
+++ backend/pkg/routehandlers/signinhandler.go
@@ -1,1 +1,49 @@
 package routehandlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"socialnetwork/pkg/controllers"
+	"socialnetwork/pkg/middleware"
+	"socialnetwork/pkg/models/readwritemodels"
+)
+
+/*
+ */
+func SignInHandler(w http.ResponseWriter, r *http.Request) {
+	//retrieve sign in form data
+	formData, ok := r.Context().Value(middleware.DataKey).(readwritemodels.ReadData)
+	if !ok {
+		http.Error(w, "failed to read form data from context", http.StatusInternalServerError)
+		return
+	}
+
+	//validate credentials
+	user, err := controllers.ValidateCredentials(formData.Data)
+	if err != nil {
+		http.Error(w, "failed to validate credentials", http.StatusUnauthorized)
+		return
+	}
+
+	//generate web token
+	token, err := controllers.CreateWebToken(user)
+	if err != nil {
+		http.Error(w, "failed to create web token", http.StatusInternalServerError)
+		return
+	}
+
+	//add token to response type, marshal and send back
+	response := readwritemodels.WriteData{
+		Status: "success",
+		Data:   token,
+	}
+
+	jsonReponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonReponse)
+}

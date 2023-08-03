a2ea0a7cedf08a1e341dc351d1a360d195599897
Peafield <peterjohncoles@gmail.com>
2023-08-03 15:23:57 +0100

    refactor(all): small edits and documentation to various functions

--- backend/pkg/controllers/signoutallusers.go
+++ backend/pkg/controllers/signoutallusers.go
@@ -6,9 +6,26 @@ import (
 	crud "socialnetwork/pkg/db/CRUD"
 )
 
+/*
+SignOutAllUsers makes sure that all users are signed out in the database.
+
+The function accetps an open database. It then will map the is_logged_in column to be affected
+and set to 0. This is the passed along with the database to the UpdateDatabaseRow function which will
+do the updating in the database.
+
+Parameters:
+  - db(*sql.DB): an open database
+
+Errors:
+  - if the database connection is not initialised.
+  - if there is an error updating the logged in status of users.
+
+Example:
+  - the function will be used on server restart.
+*/
 func SignOutAllUsers(db *sql.DB) error {
 	if db == nil {
-		return fmt.Errorf("database connection is not initialized, please run -dbopen")
+		return fmt.Errorf("database connection is not initialised, please run -dbopen")
 	}
 	affectedColumns := map[string]interface{}{}
 	affectedColumns["is_logged_in"] = 0

5e8c2910d8e2796d91243ad8cfcd9c8f6ad86734
antoniosakkal <antoniosakkal1998@gmail.com>
2023-08-09 17:41:03 +0100

    update: imported changes from different branch regarding SanitizeGroupData.go

--- backend/pkg/controllers/routecontrollers/SanitizeGroupData.go
+++ backend/pkg/controllers/routecontrollers/SanitizeGroupData.go
@@ -8,6 +8,7 @@ import (
 	"socialnetwork/pkg/db/dbstatements"
 	"socialnetwork/pkg/db/dbutils"
 	"socialnetwork/pkg/helpers"
+	"socialnetwork/pkg/models/dbmodels"
 )
 
 /**/
@@ -45,11 +46,11 @@ func SelectGroup(db *sql.DB, userId string, Conditions map[string]interface{}) (
 
 	// Check User/Group relationship
 	// If user isn't allowed access to group then return an error
-	isCreator, err := IsGroupCreator(db, userId, groupId)
+	isCreator, err := dbutils.IsGroupCreator(db, userId, groupId)
 	if err != nil {
 		return nil, fmt.Errorf("problem querying user/group relationship at SelectGroup: %v", err)
 	}
-	isMember, err := IsGroupMember(db, userId, groupId)
+	isMember, err := dbutils.IsGroupMember(db, userId, groupId)
 	if err != nil {
 		return nil, fmt.Errorf("problem querying user/group relationship at SelectGroup: %v", err)
 	}
@@ -71,49 +72,87 @@ func SelectGroup(db *sql.DB, userId string, Conditions map[string]interface{}) (
 }
 
 /**/
-func UpdateGroup(db *sql.DB, userId string, AffectedColumns map[string]interface{}) error {
+func UpdateGroup(db *sql.DB, userId, groupId string, AffectedColumns map[string]interface{}) error {
 
-	//make sure immutable parameters are not trying to be changed
-	expectedParams := []string{"title", "description"}
+	//check permission level of user
+	isPermitted, err := dbutils.IsPermitted(dbutils.DB, userId, groupId, "Groups")
+	if !isPermitted || err != nil {
+		return fmt.Errorf("user isn't permitted to update Group: %v", groupId)
+
+	}
+
+	//should maybe ignore immutable parameters received instead of returning errors
+	immutableParameters := []string{"group_id", "creator_id", "creation_date"}
 
 	//check whether the map's keys match the expected parameters
-	exists := helpers.MapKeyContains(AffectedColumns, expectedParams)
+	exists := helpers.MapKeyContains(AffectedColumns, immutableParameters)
 
-	if !exists {
-		return fmt.Errorf("parameters received = %v expected parameters = %v at InsertGroup", AffectedColumns, expectedParams)
+	if exists {
+		return fmt.Errorf("parameters received = %v expected parameters = %v at InsertGroup", AffectedColumns, immutableParameters)
 	}
 
-	uuid, err := helpers.CreateUUID()
+	// updateErr := []interface{}{uuid, AffectedColumns["title"], AffectedColumns["description"], userId}
+	err = crud.UpdateDatabaseRow(dbutils.DB, "Groups", map[string]interface{}{"group_id": groupId}, AffectedColumns)
 	if err != nil {
-		return nil
+		return err
 	}
-	insertValues := []interface{}{uuid, AffectedColumns["title"], AffectedColumns["description"], userId}
-	crud.InsertIntoDatabase(dbutils.DB, dbstatements.InsertGroupsStmt, insertValues)
-
 	return nil
 }
 
 /**/
-func DeleteGroup(db *sql.DB, userId string, AffectedColumns map[string]interface{}) error {
-	// conditions := make(map[string]interface{})
-	// conditions["user_id"] = userId
+func DeleteGroup(db *sql.DB, userId, groupId string) error {
 
-	//make sure immutable parameters are not trying to be changed
-	expectedParams := []string{"title", "description"}
-
-	//check whether the map's keys match the expected parameters
-	exists := helpers.MapKeyContains(AffectedColumns, expectedParams)
-
-	if !exists {
-		return fmt.Errorf("parameters received = %v expected parameters = %v at InsertGroup", AffectedColumns, expectedParams)
+	isCreator, err := dbutils.IsGroupCreator(db, userId, groupId)
+	if !isCreator || err != nil {
+		return fmt.Errorf("user doesn't have permission to delete group: %s", groupId)
 	}
 
-	uuid, err := helpers.CreateUUID()
+	//delete group and all related info: events, members, posts, comments, likes&dislikes
+	err = crud.DeleteFromDatabase(dbutils.DB, "Groups", map[string]interface{}{"group_id": groupId})
 	if err != nil {
-		return nil
+		return err
 	}
-	insertValues := []interface{}{uuid, AffectedColumns["title"], AffectedColumns["description"], userId}
-	crud.InsertIntoDatabase(dbutils.DB, dbstatements.InsertGroupsStmt, insertValues)
+	err = crud.DeleteFromDatabase(dbutils.DB, "Groups_Members", map[string]interface{}{"group_id": groupId})
+	if err != nil {
+		return err
+	}
+	err = crud.DeleteFromDatabase(dbutils.DB, "Groups_Events", map[string]interface{}{"group_id": groupId})
+	if err != nil {
+		return err
+	}
+	err = crud.DeleteFromDatabase(dbutils.DB, "Groups_Events_Attendees", map[string]interface{}{"group_id": groupId})
+	if err != nil {
+		return err
+	}
+
+	//select the post id and comments id to delete the likes and dislikes associated with it
+	PostsData, err := crud.SelectFromDatabase(dbutils.DB, "Posts", "SELECT * FROM Posts WHERE group_id = ?", []interface{}{groupId})
+	if err != nil {
+		return err
+	}
+	//for each post delete the related comments and likes/dislikes associated with it
+	for _, post := range PostsData {
+		//assign a variable for the postId
+		postId := (post.(dbmodels.Post)).PostId
+		err = crud.DeleteFromDatabase(dbutils.DB, "Posts", map[string]interface{}{"post_id": postId})
+		if err != nil {
+			return err
+		}
+		err = crud.DeleteFromDatabase(dbutils.DB, "Comments", map[string]interface{}{"post_id": postId})
+		if err != nil {
+			return err
+		}
+		err = crud.DeleteFromDatabase(dbutils.DB, "reactions", map[string]interface{}{"post_id": postId})
+		if err != nil {
+			return err
+		}
+
+	}
+
+	err = crud.DeleteFromDatabase(dbutils.DB, "Posts", map[string]interface{}{"group_id": groupId})
+	if err != nil {
+		return err
+	}
 
 	return nil
 }

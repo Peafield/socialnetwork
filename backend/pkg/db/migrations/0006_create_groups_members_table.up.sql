4b0694388dd54c9266b0725e6c6326a537a4471e
antoniosakkal <antoniosakkal1998@gmail.com>
2023-08-09 17:12:37 +0100

    migrate(new table): added a new table reponsible for the invitations of users to join a group

--- backend/pkg/db/migrations/0006_create_groups_members_table.up.sql
+++ backend/pkg/db/migrations/0006_create_groups_members_table.up.sql
@@ -1,7 +1,6 @@
 CREATE TABLE Groups_Members (
   group_id TEXT NOT NULL,
   member_id TEXT NOT NULL,
-  request_pending INTEGER NOT NULL DEFAULT 0,
   creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY(group_id) REFERENCES Groups(group_id),
   FOREIGN KEY(member_id) REFERENCES Users(user_id)
---
+++ backend/pkg/db/migrations/0015_create_groups_invitations_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Groups_Invitations (
+  group_id TEXT NOT NULL, 
+  user_id TEXT NOT NULL, 
+  is_invited BOOL, 
+  FOREIGN KEY(group_id) REFERENCES Groups(group_id), 
+  FOREIGN KEY(user_id) REFERENCES Users(user_id)
+);

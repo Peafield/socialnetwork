ee58d7dc97f40e9a360ac3e2f82c15b13ffba0db
Peafield <peterjohncoles@gmail.com>
2023-08-14 20:00:48 +0100

    feat(frontend): modal component to be used for profile, single post etc.

--- frontend/socialnetwork/src/components/feedback/Snackbar.tsx
+++ frontend/socialnetwork/src/components/feedback/Snackbar.tsx
@@ -59,6 +59,7 @@ const Snackbar: React.FC<SnackbarProps> = ({
     boxShadow:
       "rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px",
     cursor: "pointer",
+    transition: "all 0.4s ease",
   };
 
   const iconStyle: CSSProperties = {
